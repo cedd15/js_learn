da7a1e55ce5013af6109d89d6c956e330631b6f7
Cedd <cedricvincenth@yahoo.com>
2021-01-24 08:25:06 +0800

    added rock-paper-scissors game

---
+++ rock-paper-scissors/game.js
@@ -0,0 +1,70 @@
+const getUserChoice = userInput => {
+    userInput =  userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+      return userInput;
+    } else {
+        document.write('Invalid input.')
+      }
+  };
+
+
+
+  const getComputerChoice = ()  => {
+    randomNumber = Math.floor(Math.random() * 3);
+      switch (randomNumber) {
+        case 0:
+          return 'rock'
+            break;
+        case 1:
+          return 'paper'
+            break;
+        case 2:
+          return 'scissors'
+            break;
+      } 
+      
+  
+  }
+  
+  
+  const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
+      return 'The game was a tie.';
+    } 
+  
+    if (userChoice === 'rock') {
+      if (computerChoice === 'paper') {
+        return 'Computer won :(';
+      } else {
+        return 'You won! XD';
+      }
+    }
+  
+   if (userChoice === 'paper') {
+      if (computerChoice === 'scissors') {
+        return 'Computer won :(';
+      } else {
+        return 'You won! XD';
+      }
+    }
+  
+    if (userChoice === 'scissors') {
+      if (computerChoice === 'rock') {
+        return 'Computer won :(';
+      } else {
+        return 'You won! XD';
+      }
+    }
+  }
+  
+  
+  
+  const playGame = () => {
+    const userChoice = getUserChoice(window.prompt("Enter your choice: "));
+    const computerChoice = getComputerChoice();
+    document.write("You chose: " + userChoice + " \n");
+    document.write("The computer chose: " + computerChoice + " \n");
+    document.write(determineWinner(userChoice, computerChoice));
+    }
+  
+  playGame(); 
