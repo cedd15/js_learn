062a3bf0436630322ae3d4bacf552e306597dde7
Cedd <cedricvincenth@yahoo.com>
2021-01-28 12:23:34 +0800

    added whale-translator proj; moved weight-calc proj to another directory

---
+++ whale-translator/script.js
@@ -0,0 +1,23 @@
+// obtains human input to be translated to Whale words
+let input = window.prompt("Enter any phrase")
+
+const vowels = ['a', 'e', 'i', 'o', 'u']
+
+let resultArray = []
+
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+    for (let vowelsIndex = 0; vowelsIndex < vowels.length; vowelsIndex++){
+        if (input[inputIndex] === vowels[vowelsIndex]) { 
+                if (input[inputIndex] === 'e') {
+                    resultArray.push('ee')
+                } else if (input[inputIndex] === 'u') {
+                    resultArray.push('uu')
+                } else {
+                    resultArray.push(input[inputIndex])
+                }
+            
+        }
+    }
+}
+
+document.write(resultArray.join('').toUpperCase())
