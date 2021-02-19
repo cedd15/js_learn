6ffb159f8409847171aa37cf20f24abcab92e14b
cedd <cedricvincenth@yahoo.com>
2021-02-20 06:57:24 +0800

    added another method called compareDNA()

--- mystery-organism-starter/main.js
+++ mystery-organism-starter/main.js
@@ -14,6 +14,9 @@ const mockUpStrand = () => {
 };
 
 const p1 = mockUpStrand();
+const p2 = mockUpStrand();
+const p3 = mockUpStrand();
+
 
 const pAequorFactory = (num, arr) => {
   return {
@@ -34,14 +37,27 @@ const pAequorFactory = (num, arr) => {
 
       return this.dna
     },
-    
+    compareDNA(obj) {
+      //let identical = this.dna.reduce( (acc, val, index) => val === obj.dna[index] ? acc += 1 : acc , 0)
+      let identical = 0
+      this.dna.forEach( (element, index) => element === obj.dna[index] ? identical++ : identical += 0)
+      let percentage = Math.floor(identical / this.dna.length * 100)
+      return `specimen #1 and specimen #2 have ${percentage}% DNA in common.`
+    },
+
   }
 }
 
 const pAequor1 = pAequorFactory(1, p1)
-const mutated = pAequor1.mutate()
+const pAequor2 = pAequorFactory(1, p2)
+const pAequor3 = pAequorFactory(1, p3)
 
-console.log(mutated)
+//const mutated = pAequor1.compareDNA(pAequor2)
+
+//console.log(pAequor1.dna)
+//console.log(pAequor2.dna)
+//console.log(pAequor3.dna)
+console.log(pAequor1.compareDNA(pAequor2))
 
 
 
